2abb87ac2e5a15cdf60a9837fb1d49a2c1cf910d
cloudgang <35246400+CloudGang@users.noreply.github.com>
2020-06-08 16:05:52 -0400

    complete

---
+++ team-builder/src/components/Team.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Team = props => {
+
+  return (
+
+    <div>
+
+      {props.team.map(member => (
+        
+        <div className="member" key={member.id}>
+
+          <h2>{member.name}</h2>
+          <h3>{member.email}</h3>
+          <h4>{member.role}</h4>
+          <button onClick={() => props.setMemberToEdit(member)}>Edit</button>
+          <button onClick={() => props.deleteMember(member.id)}>Delete</button>
+        
+        </div>
+      
+      ))}
+
+    </div>
+
+  );
+  
+};
+
+export default Team;
